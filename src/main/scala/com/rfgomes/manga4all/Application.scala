8aa45a464a33d9be71383a329a0411ff5ec5960b
Ricardo Gomes <ipsiart@gmail.com>
2020-06-02 22:53:56 +0100

    change springboot to akka http

--- src/main/scala/com/rfgomes/manga4all/Application.scala
+++ src/main/scala/com/rfgomes/manga4all/Application.scala
@@ -1,11 +1,38 @@
 package com.rfgomes.manga4all
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{complete, concat, get, pathPrefix}
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import scala.concurrent.ExecutionContext.Implicits.global
 
-@SpringBootApplication
-class Application
+import scala.io.StdIn
 
 object Application extends App {
-  SpringApplication.run(classOf[Application]);
+  implicit val system = ActorSystem("httpController")
+  implicit val materializer = ActorMaterializer()
+
+
+  val route: Route =
+    concat(
+      get {
+        pathPrefix("hello") {
+          complete(
+            HttpEntity(
+              ContentTypes.`text/plain(UTF-8)`,
+              "hello"
+            )
+          )
+        }
+      }
+    )
+
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 }
