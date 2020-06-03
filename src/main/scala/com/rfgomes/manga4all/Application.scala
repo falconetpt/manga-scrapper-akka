ff683235e533e8fa2df8178857051766c0338d80
Ricardo Gomes <ipsiart@gmail.com>
2020-06-03 01:42:19 +0100

    change springboot to akka http

--- src/main/scala/com/rfgomes/manga4all/Application.scala
+++ src/main/scala/com/rfgomes/manga4all/Application.scala
@@ -35,8 +35,8 @@ object Application extends App with Directives with JsonSupport {
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
-  bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
+//  StdIn.readLine() // let it run until user presses return
+//  bindingFuture
+//    .flatMap(_.unbind()) // trigger unbinding from the port
+//    .onComplete(_ => system.terminate()) // and shutdown when done
 }
