cb33dfcc24ee8498b6f8211715e72023fcda13bd
Ricardo Gomes <ipsiart@gmail.com>
2020-06-03 01:52:10 +0100

    change springboot to akka http

--- src/main/scala/com/rfgomes/manga4all/Application.scala
+++ src/main/scala/com/rfgomes/manga4all/Application.scala
@@ -1,16 +1,12 @@
 package com.rfgomes.manga4all
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.{Directives, Route}
+import akka.http.scaladsl.server.{Directives, HttpApp, Route}
 import akka.stream.ActorMaterializer
 import com.rfgomes.manga4all.manga.domain.MangaInfo
 import com.rfgomes.manga4all.scrapper.Source
 import spray.json._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.io.StdIn
 
 
 
@@ -19,12 +15,25 @@ trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val mangaInfo = jsonFormat4(MangaInfo) // contains List[Item]
 }
 
-object Application extends App with Directives with JsonSupport {
+object Application extends App {
   implicit val system = ActorSystem("httpController")
   implicit val materializer = ActorMaterializer()
 
 
-  val route: Route =
+  val port: Int = sys.env.getOrElse("PORT", "8080").toInt
+  WebServer.startServer("0.0.0.0", port)
+
+  println(s"Server online at http://localhost:$port/\nPress RETURN to stop...")
+//  StdIn.readLine() // let it run until user presses return
+//  bindingFuture
+//    .flatMap(_.unbind()) // trigger unbinding from the port
+//    .onComplete(_ => system.terminate()) // and shutdown when done
+}
+
+
+object WebServer extends HttpApp with Directives with JsonSupport {
+
+  override protected def routes: Route =
     concat(
       get {
         pathPrefix("hello") {
@@ -32,11 +41,4 @@ object Application extends App with Directives with JsonSupport {
         }
       }
     )
-
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-//  StdIn.readLine() // let it run until user presses return
-//  bindingFuture
-//    .flatMap(_.unbind()) // trigger unbinding from the port
-//    .onComplete(_ => system.terminate()) // and shutdown when done
 }
